7fec318d86203c06314fc305eec0a4eecc843267
Hygor Henrique <101516308+Hygorhhss@users.noreply.github.com>
2024-05-26 17:30:51 -0300

    atualização

--- src/js.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function() {
-    const parallax = document.querySelector('.parallax-background');
-    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-});
